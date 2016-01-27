32530835eadc08085baf10ccdfd50da1961b44c4
kayladowling <kaydowling2@gmail.com>
2016-01-26 21:49:53 -0600

    (refactor) testing

--- client/www/test/e2e/dashboard.spec.js
+++ client/www/test/e2e/dashboard.spec.js
@@ -1,63 +1,7 @@
-<<<<<<< HEAD
 
 
 
 
-// describe('requests made', function() {
-
-//   afterEach(function() {
-//     mock.teardown();
-//   });
-
-//   beforeEach(function() {
-//     mock([{
-//       request: {
-//         path: '/api/medications',
-//         method: 'GET'
-//       },
-//       response: {
-//         data: [{
-//           id: 12,
-//           name: "Abilify (Aripiprazole)",
-//           dosage: "5mg",
-//           instruction: "Take one tablet by mouth every morning",
-//           reminder: "10:30AM Every Day",
-//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
-//         }, {
-//           id: 123,
-//           name: "Actiq (Fentanyl Citrate)",
-//           dosage: "5mg",
-//           instruction: "Take one tablet by mouth every morning",
-//           reminder: "10:30AM Every Day",
-//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
-//         }, {
-//           id: 1234,
-//           name: "Halcion (Triazolam)",
-//           dosage: "5mg",
-//           instruction: "Take one tablet by mouth every morning",
-//           reminder: "10:30AM Every Day",
-//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
-//         }, {
-//           id: 12345,
-//           name: "Quinidex (Quinidine)",
-//           dosage: "5mg",
-//           instruction: "Take one tablet by mouth every morning",
-//           reminder: "10:30AM Every Day",
-//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
-//         }]
-//       }
-//     }]);
-
-//     get();
-//   });
-
-//   it('can evaluate requests made', function() {
-
-//     expect(mock.requestsMade()).toEqual([{
-//       url: '/api/medications',
-//       method: 'GET'
-//     }, ]);
-//   });
 
 // describe('requests made', function() {
 
@@ -227,7 +171,6 @@
 //     }, ]);
 //   });
 
-=======
 // describe('requests made', function() {
 
 //   afterEach(function() {
@@ -284,13 +227,65 @@
 //     }, ]);
 //   });
 
->>>>>>> (refactor) testing
+// describe('requests made', function() {
+
+//   afterEach(function() {
+//     mock.teardown();
+//   });
+
+//   beforeEach(function() {
+//     mock([{
+//       request: {
+//         path: '/api/medications',
+//         method: 'GET'
+//       },
+//       response: {
+//         data: [{
+//           id: 12,
+//           name: "Abilify (Aripiprazole)",
+//           dosage: "5mg",
+//           instruction: "Take one tablet by mouth every morning",
+//           reminder: "10:30AM Every Day",
+//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
+//         }, {
+//           id: 123,
+//           name: "Actiq (Fentanyl Citrate)",
+//           dosage: "5mg",
+//           instruction: "Take one tablet by mouth every morning",
+//           reminder: "10:30AM Every Day",
+//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
+//         }, {
+//           id: 1234,
+//           name: "Halcion (Triazolam)",
+//           dosage: "5mg",
+//           instruction: "Take one tablet by mouth every morning",
+//           reminder: "10:30AM Every Day",
+//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
+//         }, {
+//           id: 12345,
+//           name: "Quinidex (Quinidine)",
+//           dosage: "5mg",
+//           instruction: "Take one tablet by mouth every morning",
+//           reminder: "10:30AM Every Day",
+//           image: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg"
+//         }]
+//       }
+//     }]);
+
+//     get();
+//   });
+
+//   it('can evaluate requests made', function() {
+
+//     expect(mock.requestsMade()).toEqual([{
+//       url: '/api/medications',
+//       method: 'GET'
+//     }, ]);
+//   });
+
 //   it('can clear requests', function() {
 //     mock.clearRequests();
 //     expect(mock.requestsMade()).toEqual([]);
 //   });
 // });
-<<<<<<< HEAD
 
-=======
->>>>>>> (refactor) testing
